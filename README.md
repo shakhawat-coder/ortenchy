4969cc1d8ce08937d397eab7981effb5a1415212
shakhawat <shakhawathossen188@gmail.com>
2024-02-20 21:38:19 +0600

    project setup

---
+++ README.md
@@ -0,0 +1,1 @@
+# ortencey-agency
---
+++ js/script.js
@@ -0,0 +1,13 @@
+// ===== jQuery operation =====
+$(function () {
+    // === counter up ===
+    $(".counter").counterUp({
+        delay: 10,
+        time: 1000,
+    });
+    // === counter up ===
+});
+// ===== jQuery operation =====
+
+
+
